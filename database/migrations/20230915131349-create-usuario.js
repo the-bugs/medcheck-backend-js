d404ad962a33982a48f277f7ba08f77e4cbff6c0
Matheus Victor <matheusvictor.salles@gmail.com>
2023-09-15 11:00:57 -0300

    feat: Add migration para Usuario

---
+++ database/migrations/20230915131349-create-usuario.js
@@ -0,0 +1,38 @@
+"use strict";
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable("Usuarios", {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      nome: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      email: {
+        unique: true,
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      senha: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable("Usuarios");
+  },
+};
