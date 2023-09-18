f12dcd3f5d42b206477aa8ee2b2f88cfcf3007e2
Matheus Victor <matheusvictor.salles@gmail.com>
2023-09-18 15:00:39 -0300

    feat: Add endpoint para criação de usuário

--- controllers/UsuarioController.js
+++ controllers/UsuarioController.js
@@ -43,6 +43,20 @@ class UsuarioController {
       return response.status(400).json({ message: "Dados incompletos." });
     }
   }
+
+  static async criarUsuario(request, response) {
+    const { nome, email, senha, funcao } = request.body;
+    if (nome && email && senha && funcao) {
+      try {
+        const usuario = await database.Usuario.create(dadosNovoUsuario);
+        return response.status(201).json(usuario);
+      } catch (error) {
+        return response.status(500).json(error.message);
+      }
+    } else {
+      return response.status(400).json({ message: "Dados incompletos." });
+    }
+  }
 }
 
 module.exports = UsuarioController;
