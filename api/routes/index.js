f27d790d9d36cf02b314587023271b858f3ddfd0
Matheus Victor <matheusvictor.salles@gmail.com>
2023-10-11 07:59:24 -0300

    style: Definção de rota

--- api/routes/index.js
+++ api/routes/index.js
@@ -2,11 +2,9 @@ const bodyParser = require("body-parser");
 const usuariosRoute = require("./usuarios");
 
 module.exports = (app) => {
-  app.use(bodyParser.json());
+  app.use(bodyParser.json(), usuariosRoute);
 
   app.get("/", (req, res) => {
     res.status(200).send({ message: "API Medcheck funcionando normalmente" });
   });
-
-  app.use(usuariosRoute);
 };
