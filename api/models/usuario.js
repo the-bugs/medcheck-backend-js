908cf69f2cc3fa22c84deb05868c7eca63bc0c2a
Matheus Victor <matheusvictor.salles@gmail.com>
2023-10-11 07:58:57 -0300

    feat: Add relacionamento de usuário e especialidade

--- api/models/usuario.js
+++ api/models/usuario.js
@@ -9,6 +9,9 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
+      this.hasMany(models.Especialidade, {
+        foreignKey: "especialidade_id",
+      });
     }
   }
   Usuario.init(
