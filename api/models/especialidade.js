207d3c524d7c0ed7af9da5fcc6bad7043d66d88e
Matheus Victor <matheusvictor.salles@gmail.com>
2023-10-18 15:45:21 -0300

    feat: Add modelo de Especialdiade

--- api/models/especialidade.js
+++ api/models/especialidade.js
@@ -1,22 +1,14 @@
 "use strict";
 const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
-  class especialidade extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models) {
-      // define association here
-      especialidade.belongsTo(models.Usuario, {
-        foreignKey: "especialidade_id",
-      });
-    }
+  class Especialidade extends Model {
+    static associate(models) {}
   }
-  especialidade.init(
+  Especialidade.init(
     {
-      nomeEspecialidade: DataTypes.STRING,
+      nome: DataTypes.STRING,
+      createdAt: DataTypes.DATE,
+      updatedAt: DataTypes.DATE,
     },
     {
       sequelize,
@@ -25,5 +17,5 @@ module.exports = (sequelize, DataTypes) => {
       freezeTableName: true,
     }
   );
-  return especialidade;
+  return Especialidade;
 };
