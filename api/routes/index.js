cd069ee5691676d8d3e2be6d4ed1eca42a8b110b
Matheus Victor <matheusvictor.salles@gmail.com>
2023-10-18 22:11:07 -0300

    refactor: Remove recurso Usuário

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,9 +1,9 @@
 const bodyParser = require("body-parser");
-const usuariosRoute = require("./usuarios");
+
 const especialidadesRoute = require("./especialidades");
 
 module.exports = (app) => {
-  app.use(bodyParser.json(), usuariosRoute, especialidadesRoute);
+  app.use(bodyParser.json(), especialidadesRoute);
 
   app.get("/", (req, res) => {
     res.status(200).send({ message: "API Medcheck funcionando normalmente" });
