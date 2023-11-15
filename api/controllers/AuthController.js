20a5a6a8c7c9c0eb08e2311c530fc9c80e7414b1
devruso <jamilsonjr@live.com>
2023-11-15 16:24:12 -0300

    fix: Corrige status de erro no login

--- api/controllers/AuthController.js
+++ api/controllers/AuthController.js
@@ -6,20 +6,19 @@ class AuthController {
         try {
             const { email, senha } = request.body;
 
-            const usuario = await database.Usuario.unscoped().findOne({ where: { email } });
+            const usuario = await database.Usuario.findOne({ where: { email } });
             const foreignModel = await database[usuario.tipo].findOne({ where: { usuarioId: usuario.id } });
-
             if (senha === usuario.senha && foreignModel) {
                 //auth ok
                 const access_token = jwt.sign({ user_id: usuario.id, email: email, tipo: usuario.tipo, id_externo: foreignModel.id }, process.env.JWT_SECRET, {
                     expiresIn: '2h'
                 });
-
                 return response.json({ auth: true, access_token });
             }
 
             response.status(500).json({ message: 'Login inválido!' });
         } catch (error) {
+            console.log(error.response ? error.response.data : error.message);
             return response.status(500).json(error.message);
         }
     }
