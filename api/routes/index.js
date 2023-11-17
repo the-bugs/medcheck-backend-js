ebbd43d97c3704445f525ca4a58ad0229df7bcd2
Vinicius Moura <viniciusmq@outlook.com>
2023-11-17 12:06:07 -0300

    feat: add prontuarios CRUD

--- api/routes/index.js
+++ api/routes/index.js
@@ -5,10 +5,11 @@ const adminRoute = require("./administradores");
 const usuarioRoute = require("./usuarios");
 const pacienteRoute = require("./pacientes");
 const medicoRoute = require("./medicos");
+const prontuarioRoute = require("./prontuarios");
 const auth = require('./auth');
 
 module.exports = (app) => {
-  app.use(bodyParser.json(), usuarioRoute, especialidadesRoute, adminRoute, pacienteRoute, medicoRoute, auth);
+  app.use(bodyParser.json(), usuarioRoute, especialidadesRoute, adminRoute, pacienteRoute, medicoRoute, prontuarioRoute, auth);
 
   app.get("/", (req, res) => {
     res.status(200).send({ message: "API Medcheck funcionando normalmente" });
