909d19756a1813ccbf47cbf3cdfcf781c29c9ad0
devruso <jamilsonjr@live.com>
2023-11-16 21:27:25 -0300

    fix: Corrige erro ao achar usuario

--- api/controllers/AuthController.js
+++ api/controllers/AuthController.js
@@ -3,25 +3,28 @@ const jwt = require('jsonwebtoken');
 
 class AuthController {
     static async login(request, response) {
+        function capitalize(string) {
+            return string.charAt(0).toUpperCase() + string.slice(1);
+        }
+
         try {
             const { email, senha } = request.body;
-
             const usuario = await database.Usuario.unscoped().findOne({ where: { email } });
-
+            let type = capitalize(usuario.tipo);
             if (!usuario) return response.status(404).json({ message: 'Usuário não encontrado!' });
 
-            const foreignModel = await database[usuario.tipo].findOne({ where: { usuarioId: usuario.id } });
+            const foreignModel = await database.Usuario.findOne({ where: { tipo: type, id: usuario.id } });
 
             if (senha === usuario.senha && foreignModel) {
                 //auth ok
                 const access_token = jwt.sign({ user_id: usuario.id, email: email, tipo: usuario.tipo, id_externo: foreignModel.id }, process.env.JWT_SECRET, {
                     expiresIn: '2h'
                 });
-
                 return response.json({ auth: true, access_token });
+            } else {
+                return response.status(500).json({ message: 'Login inválido!' });
             }
 
-            response.status(500).json({ message: 'Login inválido!' });
         } catch (error) {
             return response.status(500).json(error.message);
         }
