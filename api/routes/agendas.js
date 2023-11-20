43675e5698adaa7f802cb8c141d0ae4572a9a78c
Matheus Victor <matheusvictor.salles@gmail.com>
2023-11-20 17:20:43 -0300

    refactor: Altera autorização de rotas para Agendas

--- api/routes/agendas.js
+++ api/routes/agendas.js
@@ -1,25 +1,14 @@
 const { Router } = require("express");
 const AgendaController = require("../controllers/AgendaController");
 const authGuard = require("./middleware/auth");
+const Roles = require('../helpers/roles');
 
 const router = Router();
 
 router.get("/agendas", authGuard(), AgendaController.obterAgendas);
-router.get(
-  "/agendas/:id",
-  authGuard(),
-  AgendaController.obterAgendaPorId
-);
-router.post("/agendas", authGuard(), AgendaController.criarAgenda);
-router.put(
-  "/agendas/:id",
-  authGuard(),
-  AgendaController.atualizarAgenda
-);
-router.delete(
-  "/agendas/:id",
-  authGuard(),
-  AgendaController.excluirAgenda
-);
+router.get("/agendas/:id", authGuard(), AgendaController.obterAgendaPorId);
+router.post("/agendas", authGuard([Roles.Admin, Roles.Medico]), AgendaController.criarAgenda);
+router.put("/agendas/:id", authGuard([Roles.Admin, Roles.Medico]), AgendaController.atualizarAgenda);
+router.delete("/agendas/:id", authGuard([Roles.Admin, Roles.Medico]), AgendaController.excluirAgenda);
 
 module.exports = router;
