6a59d3f25c1987b4842f03ccbccc072734fce4cb
Matheus Victor <matheusvictor.salles@gmail.com>
2023-11-21 16:10:21 -0300

    feat: Listar todas as Agendas por ID de Médico

--- api/routes/agendas.js
+++ api/routes/agendas.js
@@ -7,6 +7,7 @@ const router = Router();
 
 router.get("/agendas", authGuard(), AgendaController.obterAgendas);
 router.get("/agendas/:id", authGuard(), AgendaController.obterAgendaPorId);
+router.get("/agendas/medicos/:id", authGuard(), AgendaController.obterAgendaPorIdMedico);
 router.post("/agendas", authGuard([Roles.Admin, Roles.Medico]), AgendaController.criarAgenda);
 router.put("/agendas/:id", authGuard([Roles.Admin, Roles.Medico]), AgendaController.atualizarAgenda);
 router.delete("/agendas/:id", authGuard([Roles.Admin, Roles.Medico]), AgendaController.excluirAgenda);
