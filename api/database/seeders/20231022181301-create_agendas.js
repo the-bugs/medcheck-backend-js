800dfdd5447640bb2c98684d6725ff5e8ab69306
Matheus Victor <matheusvictor.salles@gmail.com>
2023-11-23 10:50:52 -0300

    fix: Correção de data nas seeds

--- api/database/seeders/20231022181301-create_agendas.js
+++ api/database/seeders/20231022181301-create_agendas.js
@@ -8,21 +8,35 @@ module.exports = {
       [
         {
           idMedico: 1,
-          dataAgenda: new Date("2023-10-19"),
+          dataAgenda: new Date("2023-11-22"),
           disponivel: true,
           createdAt: new Date(),
           updatedAt: new Date(),
         },
         {
           idMedico: 2,
-          dataAgenda: new Date("2023-10-19"),
+          dataAgenda: new Date("2023-11-23"),
           disponivel: true,
           createdAt: new Date(),
           updatedAt: new Date(),
         },
         {
           idMedico: 3,
-          dataAgenda: new Date("2023-10-20"),
+          dataAgenda: new Date("2023-11-24"),
+          disponivel: true,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          idMedico: 3,
+          dataAgenda: new Date("2023-12-01"),
+          disponivel: true,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          idMedico: 3,
+          dataAgenda: new Date("2023-12-02"),
           disponivel: true,
           createdAt: new Date(),
           updatedAt: new Date(),
